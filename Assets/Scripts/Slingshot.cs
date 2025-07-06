a8b7d5072c020a2262538282e30ecf7c508e6815
Ozan Honamlioglu <ozanhonamlioglu@gmail.com>
2025-07-06 14:11:01 +0300

    projectile coded

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -1,43 +1,83 @@
-using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class Slingshot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
-    private GameObject _launchPoint;
+    [SerializeField] private GameObject prefabProjectile;
+
+    [SerializeField] public float velocityMult = 8f;
+
+    private Vector3 launchPos;
+    private GameObject projectile;
+    private bool aimingMode;
+    private GameObject launchPoint;
+    private Rigidbody projectileRigidbody;
+    private SphereCollider sphereCollider;
 
     private void Awake()
     {
         // Find the launch point GameObject in the scene
-        Transform launchPointTransform = GameObject.Find("LaunchPoint")?.transform;
-        _launchPoint = launchPointTransform != null ? launchPointTransform.gameObject : null;
-        _launchPoint?.SetActive(false);
+        var launchPointTrans = GameObject.Find("LaunchPoint").transform;
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+        
+        sphereCollider = GetComponent<SphereCollider>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-    }
+        if (!aimingMode) return;
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        _launchPoint?.SetActive(true);
-    }
+        Vector2 mousePos2D = Mouse.current.position.ReadValue();
+        float zDistanceFromCamera = -Camera.main.transform.position.z; // depth to convert screen to world
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(new Vector3(mousePos2D.x, mousePos2D.y, zDistanceFromCamera));
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        _launchPoint?.SetActive(false);
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = sphereCollider.radius;
+
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        // Move the projectile to this new position
+        var projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.linearVelocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile);
+        projectile.transform.position = launchPos;
         
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        launchPoint?.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        launchPoint?.SetActive(false);
     }
 }
